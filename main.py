1ec2f28b91bdb8ceef1e13d1c68ab400256bd5d5
Ибрагим <selmentausen@gmail.com>
2021-04-22 15:16:16 +0300

    added rest resource for user

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from data import db_session
+from flask_restful import Api
+from resources import user_resource
 
 app = Flask(__name__)
+api = Api(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
     db_session.global_init('db/data.sqlite')
+    api.add_resource(user_resource.UsersResource, '/api/users/<int:user_id>')
+    api.add_resource(user_resource.UsersListResource, 'api/users')
     app.run()
 
 
