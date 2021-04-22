151fff1c2f48f7260fa4d0038bf07ea3531db278
Ибрагим <selmentausen@gmail.com>
2021-04-22 15:57:01 +0300

    added index page and ability to register

--- data/users.py
+++ data/users.py
@@ -1,9 +1,12 @@
 import datetime
 import sqlalchemy
+from sqlalchemy_serializer import SerializerMixin
+from flask_login import UserMixin
 from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -15,3 +18,9 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+
+    def create_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,17 +1,57 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from data import db_session
+from data.users import User
+from forms.user import RegisterForm
 from flask_restful import Api
+from flask_login import LoginManager
 from resources import user_resource
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 api = Api(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/')
+def main_page():
+    return render_template('base.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.create_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
 def main():
     db_session.global_init('db/data.sqlite')
     api.add_resource(user_resource.UsersResource, '/api/users/<int:user_id>')
-    api.add_resource(user_resource.UsersListResource, 'api/users')
+    api.add_resource(user_resource.UsersListResource, '/api/users')
     app.run()
 
 
