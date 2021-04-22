7e3c60f784b9e0747714a84069309c5b8dfc68d7
Ибрагим <selmentausen@gmail.com>
2021-04-22 16:06:09 +0300

    added login and logout

--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
@@ -12,3 +12,9 @@ class RegisterForm(FlaskForm):
     about = TextAreaField("Немного о себе")
     submit = SubmitField('Войти')
 
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 from flask_restful import Api
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, login_required, logout_user
 from resources import user_resource
 
 app = Flask(__name__)
@@ -22,6 +22,27 @@ def load_user(user_id):
 @app.route('/')
 def main_page():
     return render_template('base.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -47,6 +68,7 @@ def register():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
 
 def main():
     db_session.global_init('db/data.sqlite')
